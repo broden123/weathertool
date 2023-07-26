4737df0643400e4ed2cbf0d721b9c26bc4b79488
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-07-26 17:32:53 -0500

    fully func api method and beginning of local storage set up

--- assets/script.js
+++ assets/script.js
@@ -3,26 +3,83 @@ var curcond = document.getElementById("cur-condition");
 var curtemp = document.getElementById("cur-temp");
 var curwind = document.getElementById("cur-wind");
 var curhumid = document.getElementById("cur-humid");
+var searchBtn = document.getElementById("search-btn");
+var searchHistory = [];
 
-fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&appid=44203954d378b4691a519992a5653548"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.city.name);
-    console.log(data.list[0].weather[0].description);
-    console.log(data.list[0].main.temp);
-    console.log(data.list[0].wind.speed);
-    console.log(data.list[0].main.humidity);
-
-    curcity.innerText = "Current Weather in " + data.city.name;
-    curcond.innerText =
-      "Current Condition: " + data.list[0].weather[0].description;
-    curtemp.innerText = "Current Temp: " + data.list[0].main.temp + "°F";
-    curwind.innerText = "Current Wind: " + data.list[0].wind.speed + " MPH";
-    curhumid.innerText =
-      "Current Humidity: " + data.list[0].main.humidity + "%";
-  });
+searchBtn.addEventListener("click", function () {
+  //grab text input to feed into api
+  event.preventDefault();
+  var textInput = document.getElementById("city-input");
+  var city = $(textInput).val();
+  console.log(city);
+  fetchApi(city);
+  //send input to local storage
+  searchHistory.push(city);
+  localStorage.setItem("Search-History", JSON.stringify(searchHistory));
+  //search history input feed into api
+});
+function fetchApi(city) {
+  // fetch api and return response onto page
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&units=imperial&appid=44203954d378b4691a519992a5653548"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      curcity.innerText = "Current Weather in " + data.city.name;
+      curcond.innerText =
+        "Current Condition: " + data.list[0].weather[0].description;
+      curtemp.innerText = "Current Temp: " + data.list[0].main.temp + "°F";
+      curwind.innerText = "Current Wind: " + data.list[0].wind.speed + " MPH";
+      curhumid.innerText =
+        "Current Humidity: " + data.list[0].main.humidity + "%";
+      document.getElementById("1-day").innerText = data.list[4].dt_txt;
+      document.getElementById("1-cond").innerText =
+        "Condition: " + data.list[4].weather[0].description;
+      document.getElementById("1-temp").innerText =
+        "Temp: " + data.list[4].main.temp + "°F";
+      document.getElementById("1-wind").innerText =
+        "Wind: " + data.list[4].wind.speed + " MPH";
+      document.getElementById("1-humid").innerText =
+        "Humidity: " + data.list[4].main.humidity + "%";
+      document.getElementById("2-day").innerText = data.list[12].dt_txt;
+      document.getElementById("2-cond").innerText =
+        "Condition: " + data.list[12].weather[0].description;
+      document.getElementById("2-temp").innerText =
+        "Temp: " + data.list[12].main.temp + "°F";
+      document.getElementById("2-wind").innerText =
+        "Wind: " + data.list[12].wind.speed + " MPH";
+      document.getElementById("2-humid").innerText =
+        "Humidity: " + data.list[12].main.humidity + "%";
+      document.getElementById("3-day").innerText = data.list[20].dt_txt;
+      document.getElementById("3-cond").innerText =
+        "Condition: " + data.list[20].weather[0].description;
+      document.getElementById("3-temp").innerText =
+        "Temp: " + data.list[20].main.temp + "°F";
+      document.getElementById("3-wind").innerText =
+        "Wind: " + data.list[20].wind.speed + " MPH";
+      document.getElementById("3-humid").innerText =
+        "Humidity: " + data.list[20].main.humidity + "%";
+      document.getElementById("4-day").innerText = data.list[28].dt_txt;
+      document.getElementById("4-cond").innerText =
+        "Condition: " + data.list[28].weather[0].description;
+      document.getElementById("4-temp").innerText =
+        "Temp: " + data.list[28].main.temp + "°F";
+      document.getElementById("4-wind").innerText =
+        "Wind: " + data.list[28].wind.speed + " MPH";
+      document.getElementById("4-humid").innerText =
+        "Humidity: " + data.list[28].main.humidity + "%";
+      document.getElementById("5-day").innerText = data.list[36].dt_txt;
+      document.getElementById("5-cond").innerText =
+        "Condition: " + data.list[36].weather[0].description;
+      document.getElementById("5-temp").innerText =
+        "Temp: " + data.list[36].main.temp + "°F";
+      document.getElementById("5-wind").innerText =
+        "Wind: " + data.list[36].wind.speed + " MPH";
+      document.getElementById("5-humid").innerText =
+        "Humidity: " + data.list[36].main.humidity + "%";
+    });
+}
